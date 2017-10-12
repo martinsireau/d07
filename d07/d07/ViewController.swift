d56af27ea30573b7d09a00ef617ea33f2bc5de50
Martin SIREAU <martinsireau@Martins-MacBook-Pro.local>
2017-10-12 09:19:17 +0200

    ok

--- d07/d07/ViewController.swift
+++ d07/d07/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RecastAI
+import ForecastIO
 
 class ViewController: UIViewController {
 
@@ -15,28 +16,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var myLabel: UILabel!
     
     let bot = RecastAIClient(token : "86e58d61cf0d5e00923fc2dfd506d48b", language: "fr")
-    let forecastToken = "a265eff50f4af5817a0cac58a3afa3a7"
+    let darkSkyClient = DarkSkyClient(apiKey: "a265eff50f4af5817a0cac58a3afa3a7")
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    func makeRequest(request: String){
+    
+    @IBAction func myButton(_ sender: Any) {
+        self.makeRecastRequest(request: myTextField.text!)
+      
+    }
+    
+    func makeRecastRequest(request: String){
         self.bot.textRequest(request, successHandler: { reponse in
             print("response = \(reponse)")
-            print("\n otherOne:")
             if let myRes = reponse.entities?["location"] as? [[String : Any]]{
-                print(myRes[0]["lat"] ?? "no Lat")
-                print(myRes[0]["lng"] ?? "no Lat")
+                let lat = myRes[0]["lat"] as! Double!
+                let lng = myRes[0]["lng"] as! Double!
+                
+                self.darkSkyClient.getForecast(latitude: lat!, longitude: lng!, completion: { result in
+                    switch result {
+                    case .success(let value, _):
+                        let formatted = myRes[0]["formatted"] as! String!
+                        DispatchQueue.main.async {
+                            self.myLabel.text = "\(formatted!) is \((value.hourly!.summary)!)"
+                        }
+
+                    case .failure(let error):
+                        print(error)
+                    }
+                })
             }
         }
             , failureHandle: {fail in
                 print("faillll = \(fail)")
         })
-    }
-    
-    @IBAction func myButton(_ sender: Any) {
-        self.makeRequest(request: myTextField.text!)
     }
     
 }
